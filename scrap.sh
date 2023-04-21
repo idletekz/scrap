02845def7969d3c435968f0fadbee8fb1877e42d
idletekz <idletekz@gmail.com>
2023-04-21 08:47:52 -0400

    Update scrap.sh

--- scrap.sh
+++ scrap.sh
@@ -1,12 +1,13 @@
 function update_key_if_exists() {
-  local key_name="$1"
+  local key_path="$1"
   local new_value="$2"
   local input_file="$3"
-  key_exists=$(yq "has(\"$key_name\")" "$input_file")
-  if [ "$key_exists" = "true" ]; then
-    yq -i ".$key_name=\"$new_value\"" "$input_file"
-    echo "Key '$key_name' updated"
+# This command will return the contents of the input YAML file if the key exists. If the key does not exist, the command will return nothing.
+  result=$(yq "select(.$key_path != null)" "$input_file")
+  if [ -n "$result" ]; then
+    yq -i ".$key_path=\"$new_value\"" "$input_file"
+    echo "Key '$key_path' updated"
   else
-    echo "Key '$key_name' not found in the file."
+    echo "Key '$key_path' not found in the file."
   fi
 }
