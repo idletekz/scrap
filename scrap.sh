d9052ba97c7f09f017f147e6cdafcc485b11040a
idletekz <idletekz@gmail.com>
2023-09-27 08:39:59 -0400

    Update scrap.sh

--- scrap.sh
+++ scrap.sh
@@ -26,3 +26,26 @@ indented_content=$(indent_content "$content")
 
 # Print the indented content
 echo "$indented_content"
+
+
+import yaml
+
+def text_to_yaml(filename):
+    # Read the content from the text file
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+    # Extract key-value pairs from the content
+    data_list = []
+    for line in lines:
+        key, value = line.split(',')
+        data_list.append({"id": key.strip(), "value": value.strip()})
+
+    # Convert the list of key-value pairs to a YAML formatted string
+    yaml_string = yaml.dump(data_list, default_flow_style=False)
+    
+    return yaml_string
+
+# Example usage
+filename = 'sample.txt'
+print(text_to_yaml(filename))
