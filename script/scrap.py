32fa9357a9c8b7aed02ba70d43c8866ad2594927
idletekz <idletekz@gmail.com>
2024-11-22 17:12:20 -0500

    Update scrap.py

--- script/scrap.py
+++ script/scrap.py
@@ -1,20 +1,67 @@
-import math
-from collections import Counter
+apiVersion: v1
+kind: Secret
+metadata:
+  name: redis-password
+type: Opaque
+data:
+  redis-password: xxx
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: redis-deployment
+  labels:
+    app: redis
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: redis
+  template:
+    metadata:
+      labels:
+        app: redis
+    spec:
+      containers:
+      - name: redis
+        image: redis:7.2.4
+        ports:
+        - containerPort: 6379
+        resources:
+          limits:
+            memory: "256Mi"
+            cpu: "500m"
+          requests:
+            memory: "128Mi"
+            cpu: "250m"
+        env:
+        - name: REDIS_PASSWORD
+          valueFrom:
+            secretKeyRef:
+              name: redis-password
+              key: redis-password
+        command: ["redis-server"]
+        args:
+        - "--requirepass"
+        - "$(REDIS_PASSWORD)"
+        - "--maxmemory"
+        - "2gb"
+        - "--maxmemory-policy"
+        - "allkeys-lru"
+---
 
-# Count the frequency of each unique character.
-# Calculate the probability of each character.
-# Sum the product of the probability and the logarithm of the probability.
-# Multiply by -1 to get the final entropy value.
-def shannon_entropy(string):
-    # Count the frequency of each character in the string
-    freq = Counter(string)
-    # Total number of characters in the string
-    length = len(string)
-    # Calculate the Shannon entropy
-    entropy = -sum((count / length) * math.log2(count / length) for count in freq.values())
-    return entropy
+apiVersion: v1
+kind: Service
+metadata:
+  name: redis-service
+  annotations:
+    cloud.google.com/load-balancer-type: "External"
+spec:
+  selector:
+    app: redis
+  ports:
+  - protocol: TCP
+    port: 6379
+    targetPort: 6379
+  type: LoadBalancer
 
-# Example usage:
-input_string = "hello world"
-entropy = shannon_entropy(input_string)
-print(f"Shannon entropy of '{input_string}' is: {entropy}")
