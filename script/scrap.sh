96f8b24836a9a03aaef3780dcb70e15cfda29303
idletekz <idletekz@gmail.com>
2024-11-28 16:21:41 -0500

    Update scrap.sh

--- script/scrap.sh
+++ script/scrap.sh
@@ -1,11 +1,1 @@
-az rest --method GET \
-  --url "https://graph.microsoft.com/v1.0/servicePrincipals/$spnID/transitiveMemberOf" \
-  --headers '{"Content-Type":"application/json"}'
-
-az rest --method GET \
-  --url "https://graph.microsoft.com/v1.0/directoryRoles/$roleID" \
-  --headers '{"Content-Type": "application/json"}'
-
-az rest --method GET \
-  --url "https://graph.microsoft.com/v1.0/roleManagement/directory/roleDefinitions/$roleTemplateID" \
-  --headers '{"Content-Type": "application/json"}' 
+curl -s https://api.github.com/repos/{owner}/{repo}/commits/{branch} | jq -r '.sha'
