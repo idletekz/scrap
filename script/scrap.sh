a830b9d4f1fa16ea752937dcdb87c42e28964ed5
idletekz <idletekz@gmail.com>
2025-01-08 14:10:02 -0500

    Update scrap.sh

--- script/scrap.sh
+++ script/scrap.sh
@@ -1,1 +1,20 @@
-curl -s https://api.github.com/repos/{owner}/{repo}/commits/{branch} | jq -r '.sha'
+apiVersion: v1
+kind: Policy
+metadata:
+  name: enforce-specific-namespace-if-defined
+spec:
+  rules:
+    - name: Namespace must be defined
+      errorMessage: "Namespace must be defined and set to 'my-namespace'."
+      schemaValidation:
+        - path: "metadata.namespace"
+          condition: Exists
+
+    - name: Ensure namespace is 'my-namespace'
+      errorMessage: "Namespace must be set to 'my-namespace'."
+      schemaValidation:
+        - path: "metadata.namespace"
+          condition: Equals
+          value: "my-namespace"
+
+datree test deployment.yaml --policy specific-namespace-policy.yaml
