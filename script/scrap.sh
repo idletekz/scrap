9e458728dc6143d36fdac0351cfc3fa711a670b4
idletekz <idletekz@gmail.com>
2025-01-23 07:48:55 -0500

    Update scrap.sh

--- script/scrap.sh
+++ script/scrap.sh
@@ -1,14 +1,18 @@
-apiVersion: v1
-kind: Policy
-metadata:
-  name: enforce-specific-namespace-if-defined
-spec:
-  rules:
-    - name: Ensure namespace is 'my-namespace' if defined
-      errorMessage: "Namespace must be set to 'my-namespace' if defined."
-      schemaValidation:
-        - path: "metadata.namespace"
-          condition: Equals
-          value: "my-namespace"
+# finds all images (including containers, initContainers, and ephemeralContainers) 
+# removes the leading registry/domain part if it exists (including the slash) 
+#  look for one or more characters that are not a slash (^[^/]+) followed by a slash (/), from the start of the string (^).
+#   If it matches, it replaces that portion with an empty string ("").
+#  
+yq e '
+  ..
+  | select(has("containers") or has("initContainers") or has("ephemeralContainers"))
+  | (.containers + .initContainers + .ephemeralContainers)[]?.image
+  | sub("^[^/]+/", "")
+' multiple-resources.yaml
 
-datree test deployment.yaml --policy specific-namespace-policy.yaml
+yq e '
+  ..
+  | select(has("image"))
+  | .image
+  | sub("^[^/]+/", "")
+' multiple-resources.yaml
