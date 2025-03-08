e518854cc5dce277cb93e96ca2a8d0f6dc4b4ee7
idletekz <idletekz@gmail.com>
2025-03-08 17:51:38 -0500

    Update scrap.sh

--- script/scrap.sh
+++ script/scrap.sh
@@ -1,14 +1,35 @@
-api_endpoints:
-  - name: "httpbin"
-    url: "https://httpbin.org/get"
-
-  - name: "jsonplaceholder"
-    url: "https://jsonplaceholder.typicode.com/posts/1"
-
-  - name: "portquiz"
-    host: "portquiz.net"
-    port: 80
-
-  - name: "example"
-    host: "example.net"
-    port: 8080
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: startup-script
+data:
+  entrypoint.sh: |
+    #!/bin/sh
+    set -e
+    echo "Copying executable to writable location..."
+    cp /app/my-executable /writable/my-executable
+    chmod +x /writable/my-executable
+    echo "Running the executable..."
+    exec /writable/my-executable
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: my-pod
+spec:
+  containers:
+    - name: my-container
+      image: my-image
+      command: ["/bin/sh", "/scripts/entrypoint.sh"]
+      volumeMounts:
+        - name: scripts
+          mountPath: /scripts
+        - name: writable
+          mountPath: /writable
+  volumes:
+    - name: scripts
+      configMap:
+        name: startup-script
+        defaultMode: 0755
+    - name: writable
+      emptyDir: {}
