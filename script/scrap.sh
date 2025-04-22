bf8e8c9d7575ac0f9207be36559cbb72ca12fd1d
idletekz <idletekz@gmail.com>
2025-04-22 16:20:22 -0400

    Update scrap.sh

--- script/scrap.sh
+++ script/scrap.sh
@@ -1,1 +1,11 @@
-^\s*(key|certificate):\s*\|\r?\n(.*?\r?\n)*?^\s*-----END (PRIVATE KEY|CERTIFICATE)-----\r?\n?$
+parallel -j 4 \
+  rsync -azvn \
+    -e "ssh -i /here/sshkey.pem -o StrictHostKeyChecking=no" \
+    --partial --append-verify --info=progress2 \
+    "${artifact}" {}:"${dest}" \
+  ::: "${hosts[@]}"
+
+#!/usr/bin/env bash
+artifact="/path/to/artifact.tar.gz"
+dest="/remote/path/"
+hosts=( host1 host2 host3 ... )
